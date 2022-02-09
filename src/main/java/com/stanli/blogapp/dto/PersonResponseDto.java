0d93c7dd14dfbefdc42de9a01eca51e7d1a9fd87
Sley23 <obiwonike@gmail.com>
2022-02-09 01:31:34 +0100

    Dto created

---
+++ src/main/java/com/stanli/blogapp/dto/PersonResponseDto.java
@@ -0,0 +1,15 @@
+package com.stanli.blogapp.dto;
+
+import java.util.Date;
+
+public class PersonResponseDto {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String gender;
+    private Date dateOfBirth;
+}
