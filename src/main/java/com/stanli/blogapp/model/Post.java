f0dabfe368818dfc5bf8e707501f9ac3c8f92648
Sley23 <obiwonike@gmail.com>
2022-02-11 21:07:56 +0100

    Dto created

---
+++ src/main/java/com/stanli/blogapp/model/Post.java
@@ -0,0 +1,25 @@
+package com.stanli.blogapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String intro;
+
+    private String Message;
+}
