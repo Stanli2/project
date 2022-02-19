acfc38118a4c43421127fb1c69bd6b5f2f1d5a0f
Sley23 <obiwonike@gmail.com>
2022-02-19 21:58:21 +0100

    Aunthentication

---
+++ src/main/java/com/stanli/blogapp/utils/Api_Uri.java
@@ -0,0 +1,12 @@
+package com.stanli.blogapp.utils;
+
+public class Api_Uri {
+
+    public static String [] PUBLIC_URIs = {
+            "/v2/api-docs", "/configuration/**", "/swagger*/**","/swagger-ui/**", "/webjars/**",
+            "/person/login", "/person/register",
+            "/person/admin/register", "/post/**",
+            "/post/search",
+
+    };
+}
